da80189089afd3bc9657dffd92c17de2b6588abe
Johannes Müller <straightshoota@gmail.com>
2018-10-27 13:50:40 +0200

    (WIP) Improve server, introduce Server::Handler

--- spec/server_spec.cr
+++ spec/server_spec.cr
@@ -1,13 +1,13 @@
 require "./spec_helper"
 require "../src/server"
 
-private def send_request(server : Criss::Server, url : String) : HTTP::Client::Response
+private def send_request(handler : Criss::Server::Handler, url : String) : HTTP::Client::Response
   request = HTTP::Request.new("GET", url)
   io = IO::Memory.new
   response = HTTP::Server::Response.new(io)
   context = HTTP::Server::Context.new(request, response)
 
-  server.handle(context)
+  handler.call(context)
   response.close
 
   io.rewind
@@ -18,9 +18,9 @@ describe Criss::Server do
   it do
     site = load_site("simple-site")
 
-    server = Criss::Server.new(site)
+    handler = Criss::Server::Handler.new(site)
 
-    response = send_request(server, "/2017/08/07/markdown.html")
+    response = send_request(handler, "/2017/08/07/markdown.html")
     response.success?.should be_true
     response.body.should eq File.read(File.join(site.site_dir, "/_build/2017/08/07/markdown.html"))
   end
--- src/server.cr
+++ src/server.cr
@@ -6,19 +6,85 @@ class Criss::Server
   def initialize(@site : Site)
   end
 
-  def handle(context : HTTP::Server::Context)
-    path = context.request.path
+  def start(uri : String | URI)
+    @server = server = HTTP::Server.new [
+      HTTP::ErrorHandler.new,
+      Handler.new(@site)
+    ]
 
-    p! path
-    resource = site.find(path)
-    p! resource
+    address = server.bind url
 
-    unless resource
-      context.response.respond_with_error "Not Found", code: 404
-      context.response.close
-      return
+    puts "Listening on #{address}"
+
+    server.listen
+  end
+
+  class Handler
+    include HTTP::Handler
+
+    def initialize(@site : Site)
     end
 
-    site.run_processor(context.response, resource)
+    def call(context : HTTP::Server::Context)
+      path = context.request.path
+
+      resource = @site.find(path)
+
+      unless resource
+        context.response.respond_with_error "Not Found", code: 404
+        context.response.close
+        return
+      end
+
+      @site.run_processor(context.response, resource)
+    end
   end
+
+
+  # DEFAULT_HOST = "0.0.0.0"
+  # DEFAULT_PORT = 3000
+
+  # property host : String = DEFAULT_HOST
+  # property port : Int32 = DEFAULT_PORT
+
+  # getter! server : HTTP::Server
+  # getter site : Site
+  # getter! handler : CrissHandler
+
+  # getattr host, port
+
+  # def initialize(@site)
+  # end
+
+  # def start
+  #   setup
+
+  #   url = "http://#{host}:#{port}".colorize(:cyan)
+
+  #   begin
+  #     server.bind
+  #   rescue e : Errno
+  #     STDERR.puts "Criss server could not bind to #{url}"
+  #     raise e
+  #   end
+
+  #   puts "Criss server is listening on #{url}"
+
+  #   server.listen
+  # end
+
+  # def setup
+  #   return unless @server.nil?
+
+  #   @handler = CrissHandler.new
+
+  #   handlers = [
+  #     HTTP::ErrorHandler.new,
+  #     LogPrettyHandler.new(STDOUT, colors: site.config.use_colors?),
+  #     # HTTP::StaticFileHandler.new(site.source_path, directory_listing: false),
+  #     handler,
+  #   ]
+
+  #   @server = HTTP::Server.new(host, port, handlers)
+  # end
 end
